b934a14cb0dd9e62d399ae062f66b93acc5a4c58
John Erickson <ericksonjohn08@hotmail.com>
2018-06-23 09:31:20 -0400

    Code refactor on app file. Remove message file.

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 console.log('Starting app.js');
 
-// Module Dependencies
+// 3rd Party Module Dependencies
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+
+// Local Module Dependencies
 const notes = require('./notes.js');
 
 const user = os.userInfo();
-
-// fs.appendFile('message.txt', `Hello ${user.username}`, (err) => {
-//   if (err) throw err;
-//   console.log('The "data to append" was appended to file!');
-// });
-
-console.log(`Hello my name is ${notes.john.name} and I'm ${notes.john.age} years old.`);
-
-notes.john.yearOfBirth();
-
-let total = notes.addNumbers(1, 5);
-console.log(total);
-
-let testArray = notes.arrayChunk([1, 2, 3, 4, 5], 2);
-console.log(testArray);
